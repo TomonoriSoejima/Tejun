70f83b3ad4b046d374f1e278ef31312148b96fbb
TomonoriSoejima <25199092+TomonoriSoejima@users.noreply.github.com>
2017-11-23 11:05:57 +0900

    Update client.py

--- python/client.py
+++ python/client.py
@@ -1,16 +1,41 @@
+import sys
 from elasticsearch import Elasticsearch
 
-try:
-  es = Elasticsearch(
-      ['localhost'],
-      http_auth=('elastic', 'changeme'),
-      port=9200,
-  )
+def get(filename):
+    returndata = {}
+    try:
+        fd = open(filename, 'r')
+        text = fd.read()
+        fd.close()
+        returndata = text
 
-  data = {
-    'body' : "file body"
-  }
-  es.index(index='test_index', doc_type='post', id=1, body= data)
-  print("Connected", es.info())
-except Exception as ex:
-  print ("Error:", ex)
+    except: 
+        print('could not read:', filename)
+    return returndata
+
+
+def makedoc(file):
+    try:
+      es = Elasticsearch(
+          ['localhost'],
+          http_auth=('elastic', 'changeme'),
+          port=9200,
+      )
+
+      data = {
+        'body' : get(file)
+      }
+
+      res = es.index(index='test_index', doc_type='post', id=1, body=data)
+      print(res)
+      # print("Connected", es.info())
+    except Exception as ex:
+      print ("Error:", ex)
+
+
+
+def main(file):
+    makedoc(file)
+
+if __name__ == "__main__":
+    main(sys.argv[1])
