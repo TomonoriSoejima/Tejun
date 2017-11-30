719f66ecefcbf9fda03b8a2a5bf7c63e91797576
TomonoriSoejima <25199092+TomonoriSoejima@users.noreply.github.com>
2017-11-30 10:09:01 +0900

    Update shards.counter.checker.sh

--- support/shards.counter.checker.sh
+++ support/shards.counter.checker.sh
@@ -1,4 +1,6 @@
 
+$ cat shards.counter.checker.sh
+
 file=$1
 for i in kb mb gb; do
     echo snumber of shards whose size is in $i : $(grep -c $i $file)
@@ -7,10 +9,10 @@ done
 
 example
 
-```
+
 [mbp:scripts]$ sh shards.counter.checker.sh !$
 sh shards.counter.checker.sh /Users/surfer/Downloads/nokia/diagnostics-20171129-143143/cat_shards.txt
 snumber of shards whose size is in kb : 3375
 snumber of shards whose size is in mb : 5421
 snumber of shards whose size is in gb : 1532
-```
+
