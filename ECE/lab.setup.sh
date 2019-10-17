692284df6133a00dba029cdf930855c3dc9760b5
TomonoriSoejima <25199092+TomonoriSoejima@users.noreply.github.com>
2019-10-18 08:57:01 +0900

    Update lab.setup.sh

--- ECE/lab.setup.sh
+++ ECE/lab.setup.sh
@@ -1,4 +1,11 @@
 
+# install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 
-`sudo systemctl stop firewalld`
 
+sudo usermod -aG docker $USER
+sudo chmod 777 /mnt
+
+sudo systemctl stop firewalld
+sudo service docker restart
