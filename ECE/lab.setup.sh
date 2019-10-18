fa04b15efed1c67fbf68ba6dcf93bf4efb607cb5
TomonoriSoejima <25199092+TomonoriSoejima@users.noreply.github.com>
2019-10-18 09:04:56 +0900

    Update lab.setup.sh

--- ECE/lab.setup.sh
+++ ECE/lab.setup.sh
@@ -9,3 +9,13 @@ sudo chmod 777 /mnt
 
 sudo systemctl stop firewalld
 sudo service docker restart
+
+
+# to deal with the error below
+# follow https://stackoverflow.com/questions/42889241/how-to-increase-vm-max-map-count
+
+# Checking OS max map count setting... FAILED
+#  OS setting 'vm.max_map_count' should be >= 262144, currently 65530
+
+# finally this should work!
+ bash <(curl -fsSL https://download.elastic.co/cloud/elastic-cloud-enterprise.sh) install
