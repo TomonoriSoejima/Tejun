0488e05cef627915608aa23d23591c31135401cb
Tomonori Soejima <25199092+TomonoriSoejima@users.noreply.github.com>
2022-02-16 10:33:53 +0900

    Update install.sh

--- make_vm/install.sh
+++ make_vm/install.sh
@@ -1,4 +1,10 @@
-```
+
 sudo yum install -y go
 sudo yum install -y make
-```
+
+
+# https://github.com/elastic/terraform-provider-ec test
+mkdir -p ~/development; cd ~/development
+git clone https://github.com/elastic/terraform-provider-ec
+cd terraform-provider-ec
+make install
