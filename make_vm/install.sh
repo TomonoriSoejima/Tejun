19468423683d54c92b3f449e4ea97f48c5d3b45f
Tomonori Soejima <25199092+TomonoriSoejima@users.noreply.github.com>
2022-02-16 10:49:23 +0900

    Update install.sh

--- make_vm/install.sh
+++ make_vm/install.sh
@@ -3,6 +3,10 @@ sudo yum install -y go
 sudo yum install -y make
 
 
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
 # https://github.com/elastic/terraform-provider-ec test
 mkdir -p ~/development; cd ~/development
 git clone https://github.com/elastic/terraform-provider-ec
